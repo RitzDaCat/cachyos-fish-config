6d3bb59078aeb3bd5b4a548e7ee3cad877acea33
Ritz <49771174+RitzDaCat@users.noreply.github.com>
2026-01-17 14:41:47 -0600

    Refactor cachyos-config.fish for better structure
    Refactor cachyos-config.fish for improved organization and clarity. Added sections for initial source, global settings, path management, enhanced functions, abbreviations, aliases, maintenance, and system commands.

--- cachyos-config.fish
+++ cachyos-config.fish
@@ -1,123 +1,125 @@
-﻿## Source from conf.d before our fish config
-source /usr/share/cachyos-fish-config/conf.d/done.fish
+## 1. Initial Source
+# Source cachyos-fish-config (ensure path exists to avoid errors)
+if test -f /usr/share/cachyos-fish-config/conf.d/done.fish
+    source /usr/share/cachyos-fish-config/conf.d/done.fish
+end
 
-## Set values
-## Run fastfetch as welcome message
+## 2. Global Settings
+# Use global (-g) instead of universal (-U) for static config to reduce I/O
+set -g __done_min_cmd_duration 10000
+set -g __done_notification_urgency_level low
+
+# Environment Variables
+set -gx MANROFFOPT "-c"
+set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+# Welcome Message
 function fish_greeting
     fastfetch
 end
 
-# Format man pages
-set -x MANROFFOPT "-c"
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-
-# Set settings for https://github.com/franciscolourenco/done
-set -U __done_min_cmd_duration 10000
-set -U __done_notification_urgency_level low
-
-## Environment setup
-# Apply .profile: use this to put fish compatible .profile stuff in
-if test -f ~/.fish_profile
-  source ~/.fish_profile
+## 3. Path Management
+# Using a loop to keep the config DRY (Don't Repeat Yourself)
+for bin_path in ~/.local/bin ~/Applications/depot_tools
+    if test -d $bin_path
+        if not contains -- $bin_path $PATH
+            set -p PATH $bin_path
+        end
+    end
 end
 
-# Append common directories for executable files to $PATH
-fish_add_path ~/.local/bin ~/.cargo/bin ~/Applications/depot_tools
-
-## Functions
-# Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
+## 4. Enhanced Functions
+# History expansion (!! and !$)
 function __history_previous_command
-  switch (commandline -t)
-  case "!"
-    commandline -t $history[1]; commandline -f repaint
-  case "*"
-    commandline -i !
-  end
+    switch (commandline -t)
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+    end
 end
 
 function __history_previous_command_arguments
-  switch (commandline -t)
-  case "!"
-    commandline -t ""
-    commandline -f history-token-search-backward
-  case "*"
-    commandline -i '$'
-  end
+    switch (commandline -t)
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
 end
 
-if [ "$fish_key_bindings" = fish_vi_key_bindings ];
-  bind -Minsert ! __history_previous_command
-  bind -Minsert '$' __history_previous_command_arguments
+# Keybindings
+if [ "$fish_key_bindings" = fish_vi_key_bindings ]
+    bind -Minsert ! __history_previous_command
+    bind -Minsert '$' __history_previous_command_arguments
 else
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
+    bind ! __history_previous_command
+    bind '$' __history_previous_command_arguments
 end
 
-# Fish command history
-function history
-    builtin history --show-time='%F %T '
-end
-
+# Optimized Backup
 function backup --argument filename
-    cp $filename $filename.bak
+    cp -v $filename $filename.(date +%Y%m%d_%H%M%S).bak
 end
 
-# Copy DIR1 DIR2
+# Optimized Copy with string manipulation
 function copy
-    set count (count $argv | tr -d \n)
-    if test "$count" = 2; and test -d "$argv[1]"
-        set from (echo $argv[1] | trim-right /)
-        set to (echo $argv[2])
+    set -l count (count $argv)
+    if test "$count" -eq 2; and test -d "$argv[1]"
+        set -l from (string trim -r -c / $argv[1])
+        set -l to $argv[2]
         command cp -r $from $to
     else
         command cp $argv
     end
 end
 
-## Useful aliases
-# Replace ls with eza
-alias ls='eza -al --color=always --group-directories-first --icons=always' # preferred listing
-alias la='eza -a --color=always --group-directories-first --icons=always'  # all files and dirs
-alias ll='eza -l --color=always --group-directories-first --icons=always'  # long format
-alias lt='eza -aT --color=always --group-directories-first --icons=always' # tree listing
-alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
+## 5. Abbreviations & Aliases
+# Abbreviations expand upon pressing 'Space', preventing AI stalling
+# and ensuring the real command is recorded in history.
 
-# Common use
-alias grubup="sudo grub-mkconfig -o /boot/grub/grub.cfg"
-alias fixpacman="sudo rm /var/lib/pacman/db.lck"
-alias tarnow='tar -acf '
-alias untar='tar -zxvf '
-alias wget='wget -c '
+# Search & File logic (The "Anti-Stall" fix)
+abbr -a grep 'grep --color=auto'
+abbr -a find 'find'
+
+# Modern Replacements
+alias ls='eza -al --color=always --group-directories-first --icons'
+alias la='eza -a --color=always --group-directories-first --icons'
+alias ll='eza -l --color=always --group-directories-first --icons'
+alias lt='eza -aT --color=always --group-directories-first --icons'
+
+# Navigation
+abbr -a .. 'cd ..'
+abbr -a ... 'cd ../..'
+abbr -a .... 'cd ../../..'
+
+# Maintenance & System
+abbr -a update 'sudo pacman -Syu'
+abbr -a grubup 'sudo grub-mkconfig -o /boot/grub/grub.cfg'
+abbr -a fixpacman 'sudo rm /var/lib/pacman/db.lck'
+
+# Conditional Cleanup (Safety fix)
+function cleanup
+    set -l orphans (pacman -Qtdq)
+    if test -n "$orphans"
+        sudo pacman -Rns $orphans
+    else
+        echo "No orphaned packages found."
+    end
+end
+
+# Hardware & Monitoring
 alias psmem='ps auxf | sort -nr -k 4'
-alias psmem10='ps auxf | sort -nr -k 4 | head -10'
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias ......='cd ../../../../..'
-alias dir='dir --color=auto'
-alias vdir='vdir --color=auto'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
-alias hw='hwinfo --short'                                   # Hardware Info
-alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB
-alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'          # List amount of -git packages
-alias update='sudo cachyos-rate-mirrors && sudo pacman -Syu'
-
-# Get fastest mirrors
-alias mirror="sudo cachyos-rate-mirrors"
-
-# Help people new to Arch
-alias apt='man pacman'
-alias apt-get='man pacman'
-alias tb='nc termbin.com 9999'
-
-# Cleanup orphaned packages
-alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
-
-# Get the error messages from journalctl
+alias hinfo='hwinfo --short'
 alias jctl="journalctl -p 3 -xb"
 
-# Recent installed packages
+# Package analysis
+alias big="expac -H M '%m\t%n' | sort -h | nl"
 alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+
+## 6. Shell Integration
+# Apply .profile if it exists
+if test -f ~/.fish_profile
+    source ~/.fish_profile
+end
